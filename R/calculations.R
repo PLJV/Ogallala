aa3d5cd39549676d6ef5e466b37bfb3dbe0bf238
ktaylora <kyle.a.taylor@gmail.com>
2017-03-31 16:41:28 -0600

    house keeping

--- R/calculations.R
+++ R/calculations.R
@@ -9,23 +9,34 @@ generateTargetRasterGrid <- function(s=NULL) {
                          crs=sp::CRS("+init=epsg:2163"))
   return(grid)
 }
-#' using ofr98-393, make a contour line to raster product
+#' using ofr98-393, make a contour line to raster product. The units on elevation are US feet.
 #' @export
-generateBaseElevationRaster <- function(s=NULL){
-  # contourRaster <- raster(xmn=contourBBox[1,1],
-  #                       xmx=ceiling(contourBBox[1,2]),
-  #                       ymn=contourBBox[2,1],
-  #                       ymx=ceiling(contourBBox[2,2]),
-  #                       crs = CRS("+init=epsg:26918"),
-  #                       resolution = c(12,12))
-  # grid_pts <- as(s, 'SpatialPointsDataFrame')
-  # g <- gstat::gstat(id="ELEV", formula = ELEV~1, data=p, nmax=7, set=list(idp = .5))
-  # interpDEM <- gstat::interpolate(contourRaster, g) # idw interpolation
-  return(NA)
+generateBaseElevationRaster <- function(s=NULL,targetRasterGrid=NULL){
+  # sanity-check
+  if(sum(grepl(tolower(names(s)),pattern="elev"))==0) stop("no ELEV field found in the s= base countour shapefile provided.")
+  if(is.null(targetRasterGrid)){
+    cat(" -- no target raster grid specified. Will generate one from s= elevation data.\n")
+    targetRasterGrid <- generateTargetRasterGrid(s=s)
+  }
+
+  grid_pts <- as(s, 'SpatialPointsDataFrame')
+         g <- gstat::gstat(id="ELEV", formula = ELEV~1, data=grid_pts, nmax=7, set=list(idp = .5))
+
+  bedrock <- interpolate(targetRasterGrid, g)
+  return(bedrock)
 }
 #' calculate saturated thickness for a series of well points and the base elevation of
 #' the aquifer (as returned by ogallala::generateBaseElevationRaster())
 #' @export
 calculateSaturatedThickness <- function(wellPts=NULL,baseRaster=NULL){
-
+  # sanity-check our input
+  if(is.null(wellPts)) stop("wellPts= needs to be a SpatialPointsDataFrame specifying
+                             well point data, as returned by unpack_wl_zip()")
+  if(is.null(baseRaster)){
+    baseRaster = ogallala::generateBaseElevationRaster(s=wellPts)
+  } else if(!inherits(baseRaster,'raster')){
+    stop("baseRaster= argument must be a raster object, as returned by generateBaseElevationRaster()")
+  }
+  # calculate saturated thickness
+  wellPts$
 }
