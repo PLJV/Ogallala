97576e2e400eceb51eefe76fb4be536170d846a9
Kyle Taylor <kyle.a.taylor@gmail.com>
2017-05-23 20:01:58 -0600

    house keeping

---
+++ R/visualizations.R
@@ -0,0 +1,17 @@
+#' plot the z-standardized residual error for a model run
+#' @export
+plotResiduals <- function(pts){
+  require(ggplot2)
+  require(RColorBrewer)
+  pts$lon <- pts@coords[,1]
+  pts$lat <- pts@coords[,2]
+  # compress the distribution so that outliers really stand out
+  #div <- round(diff(quantile((pts$residls),na.rm=T,p=c(0.5,0.75))))
+  div <- sd(pts$residls,na.rm=T) # ~Z-score is easier to explain
+  pts$res_plt <- (pts$residls-mean(pts$residls,na.rm=T))/div
+  ggplot(pts@data, aes(x=lon, y=lat, color=res_plt)) +
+    geom_point(size=1.5, alpha=0.95) +
+    geom_point(shape = 1,size = 1.5, color = "white", alpha=0.5) +
+      xlab("longitude")+ ylab("latitude") + labs(color="Residuals (Z-score)") +
+        scale_color_gradient(low="Red", high="Green")
+}
